542d60e1ce9d19a57170cce69dd4bbbf5924f83f
Christian Fischer <christian@vcodx.com>
2020-03-03 22:57:37 +0800

    add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+"use strict";
+
+module.exports = {
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module",
+    ecmaFeatures: {
+      jsx: true
+    }
+  },
+  env: {
+    browser: true,
+    es6: true
+  },
+  plugins: ["gridsome"],
+  extends: [
+    "prettier",
+    "plugin:gridsome/recommended",
+    "plugin:vue/essential"
+  ],
+  rules: {
+    "gridsome/format-query-block": "warn",
+    "gridsome/require-g-image-src": "error",
+    "gridsome/require-g-link-to": "warn"
+  }
+};
